6a3a9f8e45d2a475b044d4bf436cb5c57192e76b
Mehmet <kocaeliproje@gmail.com>
2022-08-20 16:55:42 +0300

    Ödev

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace donguler
+{
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        //while
+        // 1 den başlayarak consoldan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp consola yazdıran program
+        Console.Write("Lütfen Bir Sayı Giriniz");
+        int sayi =int.Parse(Console.ReadLine());
+        int sayac =1;
+        int toplam= 0;
+       while (sayac<=sayi)
+       {
+        toplam += sayac;
+        sayac ++;
+        }
+        Console.WriteLine(toplam/sayi) ;
+        // a dan z ye kadar olan tüm harfleri consol a yazdır.
+        char character ='a';
+        while(character<='z')
+        {
+            Console.Write(character);
+            character ++;
+        }
+        Console.Write("****foreach****");
+        string [] arabalar = {"bmw","audi","toyota"};
+   foreach (var araba in arabalar)
+        {
+            Console.Write(arabalar);
+        }
+    }
+}
+
+}
